322df2964dfb11c7eb5009ed0c617eef9f51ba95
Nano Bacano <ricardo.bentin@gmail.com>
2018-06-18 16:22:20 -0400

    got my computer to guess a random letter by creating an array of the alphabet and the having it guess a random number between 0 and 25 and then passing that as an array index to store as computer guess

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+// Create an array of all of the letters in the alphabet. 
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// Create a function to generate a random number between 0 and 25 to correspond to each letter of the alphabet
+var computerGuess = letters[Math.floor(Math.random()* 26)]

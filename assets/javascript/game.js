bc6010e8408af45eaea7ba489154d85abd24b815
Nano Bacano <ricardo.bentin@gmail.com>
2018-06-20 12:48:15 -0400

    got the logic for the guessses to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,15 +15,31 @@ var computerGuess = letters[Math.floor(Math.random()* letters.length)];
 document.querySelector("#cp").innerHTML=computerGuess; //delete before you submit!
 // need to write logic to store the user's guess into an array and then use create a for loop that will loop 9 times and write the output into the #playerGuesses div 
 //until the user runs out of guesses an then it will clear the div, and the game will start over
-
     document.onkeyup = function(event) {
+        if (guessesLeft>0){
         guessesLeft--;
-    // Determines which key was pressed.
-    var userGuess = event.key;
-    guessedLetters.push(userGuess);
-    document.querySelector("#playerGuesses").innerHTML= guessedLetters;
+        document.querySelector("#guesses").innerHTML=guessesLeft;
+        // Determines which key was pressed.
+        var userGuess = event.key;
+        guessedLetters.push(userGuess);
+        document.querySelector("#playerGuesses").innerHTML= guessedLetters;
+    }
+        else if (guessesLeft === 0) {
+            wins = 0;
+            losses = 0;
+            guessesLeft= 9;
+            guessedLetters = [];
+            // print everything to the page
+            document.querySelector("#wins").innerHTML=wins;
+            document.querySelector("#losses").innerHTML=losses;
+            document.querySelector("#guesses").innerHTML=guessesLeft;
+            document.querySelector("#playerGuesses").innerHTML= guessedLetters;
+            alert("Please Press Any OK To Play Again!");
+            var computerGuess = letters[Math.floor(Math.random()* letters.length)];
+            document.querySelector("#cp").innerHTML=computerGuess; //delete before you submit!
+        }
    }
-//
+// might need to put the stuff inside of the document.onkeyup in a while loop
  //once guess logic with the for loop is fixed, you need to write the conditional logic for how a user wins or loses 
 
 
